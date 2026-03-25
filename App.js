59ba4b0de2361a15aa390647b6998536832b2f4c
lonnrothrobert <robert.lonnroth@ha.ax>
2026-03-25 19:43:07 +0200

    prevent Countables with no/same name, sort Countables by anount, can't add to other countable with buttons.

--- App.js
+++ App.js
@@ -14,14 +14,36 @@ import { SafeAreaView } from "react-native-safe-area-context";
 export default function App() {
   const [countables, setCountables] = useState([]);
 
-  const changeCount = (amount, index) => {
-    const newState = [...countables];
-    newState[index].count += amount;
+  const changeCount = (amount, name) => {
+    const newState = countables.map((c) => {
+      if (c.name === name) {
+        return { ...c, count: c.count + amount };
+      }
+      return c;
+    });
     setCountables(newState);
   };
 
   const addNewCountable = (name) => {
+    const trimmedName = name.trim();
+    if (trimmedName === "") {
+      alert("namnet får int vara tomt!");
+      return;
+    }
+    const exists = countables.some(
+      (c) => c.name.toLowerCase() === trimmedName.toLowerCase(),
+    );
+    if (exists) {
+      alert("namnet upptaget.");
+      return;
+    }
     const newState = [...countables, { name, count: 0 }];
+    setCountables(newState);
+  };
+
+  const deleteCountable = (index) => {
+    const newState = countables.filter((_, i) => i !== index);
+
     setCountables(newState);
   };
 
@@ -39,6 +61,7 @@ export default function App() {
     saveCountables(countables);
   }, [countables]);
 
+  const sortedCountables = [...countables].sort((a, b) => b.count - a.count);
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
@@ -46,11 +69,12 @@ export default function App() {
         style={styles.container}
       >
         <ScrollView>
-          {countables.map((countable, index) => (
+          {sortedCountables.map((countable, index) => (
             <CountableRow
               countable={countable}
               key={countable.name}
               changeCount={changeCount}
+              deleteCountable={deleteCountable}
               index={index}
             />
           ))}
