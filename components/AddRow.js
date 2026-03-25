04e9fe943805bc1ba7e0080eac284b4bc45659ad
lonnrothrobert <robert.lonnroth@ha.ax>
2026-03-25 19:32:53 +0200

    aligned Add button to right, after Add cleared text and dismiss keyboard.

--- components/AddRow.js
+++ components/AddRow.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, TextInput } from "react-native";
+import { View, TextInput, Keyboard } from "react-native";
 import { CommonStyles } from "../styles/CommonStyles";
 import { CountButton } from "./CountButton";
 
@@ -7,12 +7,24 @@ export const AddRow = ({ addNewCountable }) => {
   const [name, setName] = useState("");
 
   return (
-    <View style={CommonStyles.row}>
-      <TextInput placeholder="Enter name" onChangeText={setName} />
+    <View style={[CommonStyles.row, { alignItems: "center, gap: 10" }]}>
+      <TextInput
+        style={{
+          flex: 1,
+          borderBottomWidth: 1,
+          borderColor: "lightblue",
+          padding: 8,
+        }}
+        placeholder="Enter name"
+        value={name}
+        onChangeText={setName}
+      />
       <CountButton
         text="Add"
         submit={() => {
           addNewCountable(name);
+          setName("");
+          Keyboard.dismiss();
         }}
       />
     </View>
